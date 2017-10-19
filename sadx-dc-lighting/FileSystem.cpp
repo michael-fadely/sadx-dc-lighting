923f76a7cf8785c2d76c9f8a83b01c1a457eecbd
Michael Fadely <michaelwfadely@gmail.com>
2017-10-18 21:10:29 -0600

    cleaned up some stuff in FileSystem - that was pretty bad actually

--- sadx-dc-lighting/FileSystem.cpp
+++ sadx-dc-lighting/FileSystem.cpp
@@ -44,7 +44,7 @@ bool filesystem::remove_all(const std::string& path)
 			continue;
 		}
 
-		std::string file_path(move(combine_path(path, find_data.cFileName)));
+		const std::string file_path(move(combine_path(path, find_data.cFileName)));
 
 		if (find_data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
 		{
@@ -52,13 +52,13 @@ bool filesystem::remove_all(const std::string& path)
 		}
 		else
 		{
-			remove(file_path.c_str());
+			remove(file_path);
 		}
 
 	} while (FindNextFileA(find_handle, &find_data) == TRUE);
 
 	FindClose(find_handle);
-	return remove(path.c_str());
+	return remove(path);
 }
 
 bool filesystem::remove(const std::string& path)
@@ -78,7 +78,6 @@ bool filesystem::remove(const std::string& path)
 
 std::string filesystem::get_directory(const std::string& path)
 {
-	std::string result;
 	const auto npos = path.npos;
 
 	auto slash = path.find_last_of('\\');
@@ -88,15 +87,13 @@ std::string filesystem::get_directory(const std::string& path)
 
 		if (slash == npos)
 		{
-			result = "";
-			return result;
+			return std::string();
 		}
 	}
 
 	if (slash != path.size() - 1)
 	{
-		result = path.substr(0, slash);
-		return result;
+		return path.substr(0, slash);
 	}
 
 	const auto last = slash;
@@ -109,12 +106,10 @@ std::string filesystem::get_directory(const std::string& path)
 
 	if (slash == npos)
 	{
-		result = "";
-		return result;
+		return std::string();
 	}
 
-	result = path.substr(last);
-	return result;
+	return path.substr(last);
 }
 
 bool filesystem::create_directory(const std::string& path)
@@ -124,7 +119,6 @@ bool filesystem::create_directory(const std::string& path)
 
 std::string filesystem::get_base_name(const std::string& path)
 {
-	std::string result;
 	const auto npos = path.npos;
 
 	auto slash = path.find_last_of('\\');
@@ -133,15 +127,13 @@ std::string filesystem::get_base_name(const std::string& path)
 		slash = path.find_last_of('/');
 		if (slash == npos)
 		{
-			result = path;
-			return result;
+			return path;
 		}
 	}
 
 	if (slash != path.size() - 1)
 	{
-		result = path.substr(++slash);
-		return result;
+		return path.substr(++slash);
 	}
 
 	const auto last = slash - 1;
@@ -152,8 +144,12 @@ std::string filesystem::get_base_name(const std::string& path)
 		slash = path.find_last_of('/', last);
 	}
 
-	result = (!slash || slash == npos) ? "" : path.substr(slash + 1, last - slash);
-	return result;
+	if (!slash || slash == npos)
+	{
+		return std::string();
+	}
+
+	return path.substr(slash + 1, last - slash);
 }
 
 void filesystem::strip_extension(std::string& path)
@@ -189,26 +185,25 @@ std::string filesystem::get_working_directory()
 
 	if (length < 1)
 	{
-		return "";
+		return std::string();
 	}
 
 	const auto buffer = new char[length];
 	GetCurrentDirectoryA(length, buffer);
-	std::string str(buffer);
+	std::string result(buffer);
 	delete[] buffer;
-	return str;
+	return result;
 }
 
 std::string filesystem::combine_path(const std::string& path_a, const std::string& path_b)
 {
-	char buffer[MAX_PATH] = {};
-	const auto result = PathCombineA(buffer, path_a.c_str(), path_b.c_str());
+	char buffer[MAX_PATH] {};
+	const LPSTR result = PathCombineA(buffer, path_a.c_str(), path_b.c_str());
 
 	if (result == nullptr)
 	{
-		return "";
+		return std::string();
 	}
 
-	std::string str(result);
-	return str;
+	return std::string(result);
 }
